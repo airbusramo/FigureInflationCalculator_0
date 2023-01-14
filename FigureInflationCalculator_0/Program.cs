331d7c68d2b4d0984c84f810d7e1291ab9557ed0
omarr <omarr@AIRBUSRAMO>
2023-01-14 03:46:50 +0200

    Add project files.

---
+++ FigureInflationCalculator_0/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("This is a program for calculating the difference in price due to changes in the JPY EGP exchange rate for my figures.");
+Console.WriteLine("Type in each figures price:");
